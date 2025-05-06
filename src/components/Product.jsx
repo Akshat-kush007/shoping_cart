198677608a929e6f0c06cabbda0b15063eff6c38
Akshat-kush007 <akshatkush7@gmail.com>
2025-05-06 22:54:16 +0530

    fixed error

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,56 +1,59 @@
 import toast from "react-hot-toast";
 import { useDispatch, useSelector } from "react-redux";
-import { remove,add } from "../redux/Slices/CartSlice";
+import { remove, add } from "../redux/Slices/CartSlice";
 
-function Product({post}){
-
-  const {cart} = useSelector((state)=>state);
+function Product({ post }) {
+  const { cart } = useSelector((state) => state);
   const dispatch = useDispatch();
 
-  function addToCart(){
+  function addToCart() {
     dispatch(add(post));
-    toast.success("Item added to Cart")
+    toast.success("Item added to Cart");
   }
 
-  function removeFromCart(){
-    
+  function removeFromCart() {
     dispatch(remove(post.id));
-    toast.error("Item removed from Cart")
+    toast.error("Item removed from Cart");
   }
-  
+
   return (
-    <div className="flex flex-col items-center justify-between hover:scale-110 transition duration-300 ease-in shadow-[0_8px_30px_rgb(0,0,0,0.12)] hover:shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] gap-3 p-4 mt-10 sm:ml-5  rounded-xl">
+    <div className="flex flex-col items-center justify-between hover:scale-110 transition duration-300 ease-in shadow-[0_8px_30px_rgb(0,0,0,0.12)] hover:shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] gap-3 p-4 mt-10 sm:ml-5 rounded-xl">
       
       <h3 className="text-gray-700 font-semibold text-lg text-left truncate w-40 mt-1">
         {post.title}
       </h3>
 
-    
-      <p className="w-40 text-gray-400 font-normal text-[10px] text-left">{post.description.split(" ").slice(0,10).join(" ")+"..."}</p>
-      
+      <p className="w-40 text-gray-400 font-normal text-[10px] text-left">
+        {post.description.split(" ").slice(0, 10).join(" ") + "..."}
+      </p>
 
       <div className="h-[180px]">
-        <img src={post.image}  className="h-full w-full" />
+        <img src={post.image} alt={post.title} className="h-full w-full" />
       </div>
 
       <div className="flex justify-between gap-12 items-center w-full mt-5">
-        <p className="text-green-600 font-semibold"> ${post.price}</p>
-      
+        <p className="text-green-600 font-semibold">${post.price}</p>
+
         {
-          cart.some((p)=>p.id==post.id) ? 
-          (<button onClick={removeFromCart}
-          className="text-grey-700 border-2  border-gray-700 font-semibold rounded-full text-[12px] tracking-wide uppercase px-3 py-1 hover:text-white hover:bg-gray-700">
-            Remove Item
-          </button>) : 
-          (<button onClick={addToCart}
-            className="text-grey-700 border-2  border-gray-700 font-semibold rounded-full text-[12px] tracking-wide uppercase px-3 py-1 hover:text-white hover:bg-gray-700">
-            Add to Cart
-          </button>)
+          cart.some((p) => p.id === post.id) ? (
+            <button
+              onClick={removeFromCart}
+              className="text-grey-700 border-2 border-gray-700 font-semibold rounded-full text-[12px] tracking-wide uppercase px-3 py-1 hover:text-white hover:bg-gray-700"
+            >
+              Remove Item
+            </button>
+          ) : (
+            <button
+              onClick={addToCart}
+              className="text-grey-700 border-2 border-gray-700 font-semibold rounded-full text-[12px] tracking-wide uppercase px-3 py-1 hover:text-white hover:bg-gray-700"
+            >
+              Add to Cart
+            </button>
+          )
         }
-      
       </div>
     </div>
   );
-};
+}
 
 export default Product;
